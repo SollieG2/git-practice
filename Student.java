bc3ac3c3e82a233baaf6438ebc74bf9c75d54ea3
Sollie Garcia <sollie@hawaii.edu>
2016-10-15 02:02:06 +0000

    Add Student.java to project

---
+++ Student.java
@@ -0,0 +1,16 @@
+public class Student {
+  float   gpa;
+  String  firstName;
+  String  lastName;
+  int     age;
+  String  major;
+
+  public Student() {
+  }
+
+  public Static(String firstName, String lastName, int age) {
+    this.firstName  = firstName;
+    this.lastName   = lastName;
+    this.age        = age;
+  }
+}
