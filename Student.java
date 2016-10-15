0662e8195eb2df475af774bed44180cc9f7fd9fd
Sollie Garcia <sollie@hawaii.edu>
2016-10-15 02:17:50 +0000

    Add fullName method to Student.java.

--- Student.java
+++ Student.java
@@ -8,9 +8,13 @@ public class Student {
   public Student() {
   }
 
-  public Static(String firstName, String lastName, int age) {
+  public Student(String firstName, String lastName, int age) {
     this.firstName  = firstName;
     this.lastName   = lastName;
     this.age        = age;
+  }
+
+  public String fullName(){
+    return this.firstName + " " + this.lastName;
   }
 }
